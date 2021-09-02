7e12effa369e8cfc29183616f2a87b5baeb8ad69
Samuel <samuel-vianna@hotmail.com>
2021-09-01 22:07:08 -0300

    'dados'

---
+++ 00manipulando_dados.R
@@ -0,0 +1,33 @@
+library(readxl)
+library(dplyr)
+library(stringr)
+library(geosphere)
+
+##### médicos #####
+
+medicos <- read_xlsx('dados_raw.xlsx', sheet = 2)
+
+str_split(medicos$Local, ' ') %>% 
+  sapply(function(x) {
+    return(x[2])
+  }) -> medicos$Local
+
+head(medicos)
+
+write.table(medicos, 'medicos.csv', sep=',', row.names = F)
+
+##### pacientes #####
+
+pacientes <- read_xlsx('dados_raw.xlsx')
+head(pacientes)
+
+dist <- numeric(length(pacientes$Medico))
+
+for(i in seq_along(pacientes$Medico)){
+  ind <- grep(pacientes[i,3], LETTERS[1:7])
+  dist[i] <- distHaversine(pacientes[i,c(2,1)], medicos[ind, c(3,2)])
+}
+
+pacientes$dist <- dist / 1000
+
+write.table(pacientes, 'pacientes.csv', sep=',', row.names = F)
